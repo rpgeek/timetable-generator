96229ec52ae136b187bbc083885e2495d89b8f8f
stihl1210 <stihl1210@gmail.com>
2017-10-10 00:22:06 +0200

    added new methods for class adding

--- sandbox.py
+++ sandbox.py
@@ -2,11 +2,10 @@
 This module presents tests for timesheet generation
 """
 
-
+import datetime
 import openpyxl
 from openpyxl.styles import Alignment
 
-import datetime
 
 
 def write_day(sheet, rst, rstp, colst, colstp, weekday_str):
@@ -75,7 +74,7 @@ def write_groups(sheet, row_start, col_start):
         sheet.merge_cells(start_row=row_start,
                           start_column=col_start,
                           end_row=row_start,
-                          end_column=col_start + group - 1)
+                          end_column=col_start+group-1)
 
         col_start += group
         align(cell)
@@ -110,19 +109,98 @@ def write_hours(sheet, row_start, col_start):
         print("don't use this not for monday")
         return
     size = 19
-    input = '08:00'
+    inpt = '08:00'
     format_string = '%H:%M'
 
-    my_time = datetime.datetime.strptime(input, format_string)
+    my_time = datetime.datetime.strptime(inpt, format_string)
 
     increment = datetime.timedelta(minutes=60)
-    for i in range(size):
+    for _ in range(size):
         hours = datetime.datetime.strftime(my_time, format_string)
         print(hours)
         my_time += increment
         row_start += 1
         cell = sheet.cell(column=col_start, row=row_start, value=hours)
         align(cell)
+
+
+def add_cls_for_grp(sheet, rw_start, cl_start, grps_lst, start_hour, stop_hour, data):
+    """
+    add classes for group list
+    :param sheet: current sheet
+    :param rw_start: where to start adding (first row of class)
+    :param cl_start: first column
+    :param grps_lst: group list - should be 0/1 list of activation
+    :param start_hour: start of hour - 1-> 8.00, 2->9.00
+    :param stop_hour: as start hour
+    :param data: what to put in cell
+    """
+    col_end = cl_start
+    for ind, val in enumerate(grps_lst):
+
+        amount = GROPUS_DIVISION[ind]
+        print('amount ', amount)
+        col_end += amount
+
+        if val:
+            sheet.cell(column=cl_start, row=rw_start, value=data)
+            sheet.merge_cells(start_row=rw_start + start_hour - 1,
+                              start_column=cl_start,
+                              end_row=rw_start + stop_hour - 1,
+                              end_column=col_end-1)
+        else:
+            cl_start = col_end
+
+
+def add_classes(work_sheet, day, start_date,
+                grp_lst, start_hour, stop_hour,
+                text, repeated=False, repeat_factor=15):
+
+    """
+    Adding classes for day, can be repeatable
+    :param work_sheet: work sheet
+    :param day: which date
+    :param start_date: when timetable started
+    :param grp_lst: list of 0/1 grop
+    :param start_hour: start hour
+    :param stop_hour: stop hor
+    :param text: what text to put in cell
+    :param repeated: Boolean -> if we want to repead
+    :param repeat_factor: how many times repeat
+    """
+    date_delta = (day - start_date).days
+    if repeated:
+        for _ in range(repeat_factor):
+            day_of_std = date_delta + 1 - (date_delta // 7 * 2)
+            print('day of study', day_of_std)
+
+            row = date_delta // 7 + 2
+            print('row', row)
+
+            column = (date_delta % 7) * len(GROPUS) + 2
+            print('column', column)
+
+            row_start = (row - 2) * HOURS_MARGIN + 2
+            col_start = column
+            row_start += 4
+            add_cls_for_grp(work_sheet, row_start, col_start,
+                            grp_lst, start_hour, stop_hour, text)
+            date_delta += 7
+
+    else:
+        day_of_std = date_delta + 1 - (date_delta // 7 * 2)
+        print('day of study', day_of_std)
+
+        row = date_delta // 7 + 2
+        print('row', row)
+
+        column = (date_delta % 7) * len(GROPUS) + 2
+        print('column', column)
+
+        row_start = (row - 2) * HOURS_MARGIN + 2
+        col_start = column
+        row_start += 4
+        add_cls_for_grp(work_sheet, row_start, col_start, grp_lst, start_hour, stop_hour, text)
 
 
 def generate_calendar_per_day(work_sheet, day_of_study, start_date):
@@ -178,5 +256,11 @@ for i in range(5):
     generate_calendar_per_day(WORKSHEET, day_of_study2, start_date=START_DATE)
     day_of_study3 = datetime.date(2017, 10, 16 + i)
     generate_calendar_per_day(WORKSHEET, day_of_study3, start_date=START_DATE)
+
+
+add_classes(work_sheet=WORKSHEET, day=datetime.date(2017, 10, 2),
+            start_date=START_DATE, grp_lst=(1, 0, 0, 1),
+            start_hour=1, stop_hour=4,
+            repeated=True, text="Biologia")
 
 WORKBOOK.save('test.xlsx')
